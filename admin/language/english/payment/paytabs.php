0ae39782add1d3837f634615b2309ce2a60c1e5e
Aamnah Akram <hello@aamnah.com>
2015-09-10 10:20:39 +0500

    intial commit

---
+++ admin/language/english/payment/paytabs.php
@@ -0,0 +1,30 @@
+<?php
+// Heading
+$_['heading_title']         = ' Paytabs';
+
+// Text
+$_['text_payment']          = 'Payment';
+$_['text_paytabs']          = '<a href="https://www.paytabs.com/" taget="_blank"><img src="https://www.paytabs.com/theme/frontend_v2/img/logo_blue.png" height="30px" width="110px" alt="Paytabs" title="Paytabs" style="border: 1px solid #EEEEEE;" /></a>';
+
+$_['text_success']          = 'Success: You have modified Paytabs Payment module!';
+$_['text_paytabs_invoice']  = '<img src="https://www.paytabs.com/theme/frontend_v2/img/logo_blue.png" height="30px" width="110px"  alt="Paytabs Payment" title="Paytabs" style="border: 1px solid #EEEEEE;"/> Paytabs Payment</a>';
+$_['text_live']             = 'Live';
+$_['text_beta']             = 'Beta';
+
+// Configuration
+$_['entry_merchant']     = 'Merchant ID:<br /><span class="help">Paytabs account Email</span>';
+$_['entry_password']     = 'Password:<br /><span class="help">Paytabs account Password</span>';
+$_['entry_total']        = 'Total:<br /><span class="help">The checkout total the order must reach before this payment method becomes active</span>';	
+$_['entry_order_status'] = 'Order Status';		
+$_['entry_geo_zone']     = 'Geo Zone';
+$_['entry_status']       = 'Status';
+$_['entry_sort_order']   = 'Sort Order';
+
+// Buttons
+$_['button_save']        = 'Save';
+$_['button_cancel']      = 'Cancel';
+
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify payment PayPoint!';
+$_['error_merchant']     = 'Merchant ID Required!';
+?>
